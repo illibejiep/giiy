ef4b3ca4e044db0b5b8b1d371de2893995841808
Lapin Vasiliy <vlapin@yopolis.ru>
2014-01-27 19:36:45 +0400

    лэйаут админки

--- components/GiiyCRUDController.php
+++ components/GiiyCRUDController.php
@@ -2,7 +2,14 @@
 
 class GiiyCRUDController extends Controller
 {
-    public $layout='//main';
+    public $layout='//admin';
+
+    public function init()
+    {
+        Yii::app()->setLayoutPath(Yii::getPathOfAlias('giiy.views.layouts'));
+    }
+
+
     /**
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
